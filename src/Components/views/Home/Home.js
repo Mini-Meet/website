277c458436b3fba0dcaf514da31cd6236cd5093e
Henry Latham <henrylatham5555@gmail.com>
2019-11-28 14:00:23 -0300

    hide testimonials (fakes)

--- src/Components/views/Home/Home.js
+++ src/Components/views/Home/Home.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { Mixpanel } from '../../../Mixpanel';
-import {
-  Header,
-  Hero,
-  ProductShowcase,
-  Testimonials,
-  Footer,
-} from '../../blocks';
+import { Header, Hero, ProductShowcase, Footer } from '../../blocks';
 
 export default class Home extends Component {
   componentDidMount() {
@@ -19,7 +13,6 @@ export default class Home extends Component {
         <Header />
         <Hero isFirstHero />
         <ProductShowcase />
-        <Testimonials />
         <Hero isFirstHero={false} />
         <Footer page="HOME" />
       </div>
