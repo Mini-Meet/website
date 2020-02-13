e0fdb070b1b172a6aae912c07e531be46f492564
Henry Latham <henrylatham5555@gmail.com>
2020-02-13 08:25:15 -0300

    1. PAGE: Referee text changed to differentiate from Home & focus on referral motivation; 2. ELEMENT: HeroReferee removed and variant of Home created for referral flow; 3. Add loader to ReferralBlock to improve UX when referring other emails;

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -4,7 +4,6 @@ export { default as Featured } from './Featured/Featured.js';
 export { default as Footer } from './Footer/Footer';
 export { default as Header } from './Header/Header';
 export { default as Hero } from './Hero/Hero.js';
-export { default as HeroReferee } from './HeroReferee/HeroReferee';
 export { default as Image } from './Image/Image';
 export { default as ProductShowcase } from './ProductShowcase/ProductShowcase.js';
 export { default as ReferralBlock } from './ReferralBlock/ReferralBlock';
