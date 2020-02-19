e8d4e67455f72abae4d5d0a09c3b23811c8112c0
Henry Latham <henrylatham5555@gmail.com>
2020-02-19 14:12:10 +0100

    STORY: Video player for server-stored videos + close button to hide after watching

---
+++ src/components/elements/Video/Video.js
@@ -0,0 +1,82 @@
+// @flow
+import React, { Component } from 'react';
+import { toInteger } from 'lodash';
+import { Player } from 'video-react';
+import { Mixpanel } from '../../../Mixpanel';
+import { Icon } from '..';
+
+import './Video.scss';
+
+type Props = {
+  videoId: string,
+  id: string,
+  header: string,
+};
+
+type State = {
+  videoClosed: boolean,
+};
+
+export default class Video extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoTimerSet: false,
+      videoClosed: false,
+    };
+  }
+
+  componentDidMount() {
+    this.refs.player.subscribeToStateChange(this.handleVideoState.bind(this));
+  }
+
+  render() {
+    const { videoId, header, id } = this.props;
+
+    const { videoClosed } = this.state;
+
+    return (
+      <div>
+        {!videoClosed && (
+          <div className="video" id={id}>
+            <div className="video__header">
+              <div className="video__header_caption">{header}</div>
+              <Icon
+                icon="close"
+                onClick={this.handleVideoClose.bind(this)}
+                classOverride="video__header_icon"
+              />
+            </div>
+            <Player
+              ref="player"
+              playsInline
+              // poster="/assets/poster.png"
+              src={videoId}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  handleVideoClose = () => {
+    const { videoClosed } = this.state;
+    const { id } = this.props;
+
+    Mixpanel.track(`${id} / Video / ${videoClosed}`);
+
+    this.setState({
+      videoClosed: true,
+    });
+  };
+
+  handleVideoState = state => {
+    const { videoTimerSet } = this.state;
+    const { id } = this.props;
+
+    if (toInteger(state.currentTime) === 30 && !videoTimerSet) {
+      this.setState({ videoTimerSet: true });
+      Mixpanel.track(`${id} / Video / ${toInteger(state.currentTime)}`);
+    }
+  };
+}
--- src/components/elements/index.js
+++ src/components/elements/index.js
@@ -5,3 +5,4 @@ export { default as Icon } from './Icon/Icon';
 export { default as Input } from './Input/Input';
 export { default as MenuItem } from './MenuItem/MenuItem.js';
 export { default as Tag } from './Tag/Tag';
+export { default as Video } from './Video/Video';
