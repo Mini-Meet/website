5bd335e05f88dbe9f443119dc26377c66fea4ae6
Henry Latham <henrylatham5555@gmail.com>
2020-02-21 11:26:21 +0100

    ARCHIVE: Learn how to pass actions up and down to specific element of an array;

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -4,6 +4,7 @@ export { default as Home } from './Home/Home';
 export { default as Legal } from './Legal/Legal';
 export { default as Mentors } from './Mentors/Mentors';
 export { default as Privacy } from './Privacy/Privacy';
+export { default as Register } from './Register/Register';
 export { default as Referee } from './Referee/Referee';
 export { default as Referral } from './Referral/Referral';
 export { default as Register } from './Register/Register';
