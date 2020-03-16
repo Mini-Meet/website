bfcc62a9123bdcfb34136f6545ecdf3bb66c4a13
Henry Latham <henrylatham5555@gmail.com>
2020-03-16 10:57:45 +0100

    remove commented code

--- src/components/views/Checkout/Checkout.js
+++ src/components/views/Checkout/Checkout.js
@@ -1,16 +1,10 @@
 // @flow
 import React, { Component } from 'react';
 import { Mixpanel } from '../../../Mixpanel';
-// import {Elements} from '@stripe/react-stripe-js';
-// import {loadStripe} from '@stripe/stripe-js';
 
 import { Header } from '../../blocks';
 
-// import CheckoutForm from './CheckoutForm';
-
 import './Checkout.scss';
-
-// const stripePromise = loadStripe("pk_live_Tvv8mdtEo3JeO2hb7plXSirc00Sh4iQnIh");
 
 export default class Checkout extends Component<Props> {
   componentDidMount() {
