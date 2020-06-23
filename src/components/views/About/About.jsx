8e50fd1e54c59b0e616b1802a6c293981abd9846
Henry Latham <henrylatham5555@gmail.com>
2020-06-23 10:45:20 +0200

    fix About page error

--- src/components/views/About/About.jsx
+++ src/components/views/About/About.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Mixpanel } from '../../../Mixpanel';
 
 import { Header, Newsletter, Footer } from '../../blocks';
