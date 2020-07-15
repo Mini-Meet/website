5350b66e30c6e813f34bba6de01757631311c4b5
Henry Latham <henrylatham5555@gmail.com>
2020-07-15 09:18:50 +0200

    update pixel ID

--- src/FacebookPixel.js
+++ src/FacebookPixel.js
@@ -5,7 +5,7 @@ const options = {
   debug: false, // enable logs
 };
 
-ReactPixel.init('329865681748119', options);
+ReactPixel.init('2514357432204795', options);
 
 const currentEnvironment = process.env.NODE_ENV === 'production'; // development
 
