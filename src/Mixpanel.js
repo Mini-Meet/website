e2be7ead100610e6fdc1521fca5b8dd90d50e2e8
Henry Latham <henrylatham5555@gmail.com>
2021-04-07 08:17:22 -0300

    disable mixpanel

--- src/Mixpanel.js
+++ src/Mixpanel.js
@@ -1,24 +1,24 @@
-import mixpanel from 'mixpanel-browser';
+// import mixpanel from 'mixpanel-browser';
 
-mixpanel.init('87c62ae60a6aad4c516c50d0fe7ceae6');
-
-const currentEnvironment = process.env.NODE_ENV === 'production';
-
-const actions = {
-  identify: id => {
-    if (currentEnvironment) mixpanel.identify(id);
-  },
-  alias: id => {
-    if (currentEnvironment) mixpanel.alias(id);
-  },
-  track: (name, props) => {
-    if (currentEnvironment) mixpanel.track(name, props);
-  },
-  people: {
-    set: props => {
-      if (currentEnvironment) mixpanel.people.set(props);
-    },
-  },
-};
-
-export const Mixpanel = actions;
+// mixpanel.init('87c62ae60a6aad4c516c50d0fe7ceae6');
+//
+// const currentEnvironment = process.env.NODE_ENV === 'production';
+//
+// const actions = {
+//   identify: id => {
+//     if (currentEnvironment) mixpanel.identify(id);
+//   },
+//   alias: id => {
+//     if (currentEnvironment) mixpanel.alias(id);
+//   },
+//   track: (name, props) => {
+//     if (currentEnvironment) mixpanel.track(name, props);
+//   },
+//   people: {
+//     set: props => {
+//       if (currentEnvironment) mixpanel.people.set(props);
+//     },
+//   },
+// };
+//
+// export const Mixpanel = actions;
